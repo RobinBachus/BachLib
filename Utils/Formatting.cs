3fe1b5013d330fd4a3bc1212116722a7eed1ee0e
RobinBachus <robinbachus@gmail.com>
2024-10-16 19:54:16 +0200

    Add project files.

---
+++ Utils/Formatting.cs
@@ -0,0 +1,18 @@
+﻿namespace BachLib.Utils
+{
+	public static class Formatting
+	{
+		public static string DataSizeToHumanReadable(long dataSize)
+		{
+			string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+			long len = dataSize;
+			int order = 0;
+			while (len >= 1024 && order < sizes.Length - 1)
+			{
+				order++;
+				len /= 1024;
+			}
+			return $"{len:0.##} {sizes[order]}";
+		}
+	}
+}
