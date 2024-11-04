cbce6248fcf2f99db71d81bd91dcb4025553100f
RobinBachus <robinbachus@gmail.com>
2024-11-04 13:34:43 +0100

    Simplify collecion

--- Utils/Formatting.cs
+++ Utils/Formatting.cs
@@ -4,7 +4,7 @@
 	{
 		public static string DataSizeToHumanReadable(long dataSize)
 		{
-			string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+			string[] sizes = ["B", "KB", "MB", "GB", "TB"];
 			long len = dataSize;
 			int order = 0;
 			while (len >= 1024 && order < sizes.Length - 1)
