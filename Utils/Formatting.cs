a0854ec3b690616255d15d0f9b650a6d55b0ce3c
RobinBachus <robinbachus@gmail.com>
2024-11-25 20:03:08 +0100

    Improve error handling, resource management, and readability
    Simplified DataSizeToHumanReadable in Formatting.cs by removing redundant variable. Added comments, formatting adjustments, and Dispose method in Player.cs. Marked ToString as readonly and removed unnecessary #pragma directives in Metadata.cs. Enhanced error handling and resource management in Renderer.cpp with event-driven buffering and new render_audio_chunk method. Updated Renderer.h with new HANDLE render_event_ and replaced New_chunk_loaded with new_chunk_loaded boolean. Renamed mix_format to wave_format and updated Initialize method for event-driven buffering in WasapiWrapper.cpp.

--- Utils/Formatting.cs
+++ Utils/Formatting.cs
@@ -5,14 +5,13 @@
 		public static string DataSizeToHumanReadable(long dataSize)
 		{
 			string[] sizes = { "B", "KB", "MB", "GB", "TB" };
-			long len = dataSize;
 			int order = 0;
-			while (len >= 1024 && order < sizes.Length - 1)
+			while (dataSize >= 1024 && order < sizes.Length - 1)
 			{
 				order++;
-				len /= 1024;
+				dataSize /= 1024;
 			}
-			return $"{len:0.##} {sizes[order]}";
+			return $"{dataSize:0.##} {sizes[order]}";
 		}
 	}
 }
