4380e9cec95ea265820e93940cf869b77dbf5818
RobinBachus <robinbachus@gmail.com>
2024-12-12 16:41:21 +0100

    Better hanling of None loglevel + docs

--- Logging/Enums/LogLevel.cs
+++ Logging/Enums/LogLevel.cs
@@ -1,13 +1,30 @@
 ﻿namespace BachLib.Logging.Enums
 {
-    public enum LogLevel
-    {
-        Trace,
-        Debug,
-        Info,
-        Warn,
-        Error,
-        Fatal,
-        None
-    }
+	/// <summary>
+	///     The log level of a log message. A higher log level means more important.
+	/// </summary>
+	public enum LogLevel
+	{
+		/// <summary>
+		///     Most verbose log level.
+		/// </summary>
+		Trace,
+		Debug,
+
+		/// <summary>
+		///     Default log level.
+		/// </summary>
+		Info,
+		Warn,
+		Error,
+		Fatal,
+
+		/// <summary>
+		///     Do not log any messages.
+		///     <para>
+		///         Should not be used as a log level for a message.
+		///     </para>
+		/// </summary>
+		None
+	}
 }
--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -4,8 +4,14 @@ namespace BachLib.Logging
 {
 	public static class Logger
 	{
+		/// <summary>
+		///     Only log messages with a log level equal to or higher than this level.
+		/// </summary>
 		public static LogLevel Level { get; set; } = LogLevel.Info;
 
+		/// <summary>
+		///     Show the prefix in the log.
+		/// </summary>
 		public static bool ShowPrefix { get; set; } = true;
 
 		/// <summary>
@@ -17,12 +23,17 @@ namespace BachLib.Logging
 		public static bool ShowDateTime { get; set; } = true;
 
 		/// <summary>
-		///    Show the log level in the log.
+		///     Show the log level in the log.
 		/// </summary>
 		/// <remarks>
-		///    Ignored if <see cref="ShowPrefix" /> is false.
+		///     Ignored if <see cref="ShowPrefix" /> is false.
 		/// </remarks>
 		public static bool ShowLevel { get; set; } = true;
+
+		/// <summary>
+		///    Throw error logs as exceptions.
+		/// </summary>
+		public static bool ThrowErrorLogs { get; set; }
 
 		public static TextWriter Out { get; set; } = Console.Out;
 
@@ -36,10 +47,24 @@ namespace BachLib.Logging
 			{ LogLevel.Fatal, ConsoleColor.DarkRed }
 		};
 
-		public static void Log(string value, LogLevel level = LogLevel.Info)
+		/// <summary>
+		///     Log a message with the specified log level.
+		/// </summary>
+		/// <param name="value">The message to log.</param>
+		/// <param name="level">The log level of the message.</param>
+		/// <param name="force">Force the message to be logged, even if the log level is lower than the specified level.</param>
+		/// <remarks>
+		///     If the log level is set to <see cref="LogLevel.None" />, the message will not be printed.
+		///     To suppress the warning, use <paramref name="force" />.
+		/// </remarks>
+		public static void Log(string value, LogLevel level = LogLevel.Info, bool force = false)
 		{
-			if (level < Level)
-				return;
+			switch (force)
+			{
+				case false when level == LogLevel.None:
+				case false when level < Level:
+					return;
+			}
 
 			// Print newlines before the prefix
 			if (value.StartsWith('\n'))
@@ -53,6 +78,8 @@ namespace BachLib.Logging
 
 			LogPrefix(level);
 			Out.WriteLine(value);
+			if (ThrowErrorLogs && level == LogLevel.Error)
+				throw new Exception(value);
 		}
 
 		private static void LogPrefix(LogLevel level)
