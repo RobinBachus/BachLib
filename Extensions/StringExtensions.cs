88982f4ed9ebb4220ca54cb6a53014510432da4a
RobinBachus <robinbachus@gmail.com>
2025-01-05 01:49:23 +0100

    Update README + string extensions

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace BachLib.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string ToTitleCase(this string? input)
+		{
+			return string.IsNullOrEmpty(input)
+				? string.Empty
+				: CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
+		}
+
+		public static string ToLowerTrim(this string? input)
+		{
+			return string.IsNullOrEmpty(input)
+				? string.Empty
+				: input.ToLower().Trim();
+		}
+
+		/**
+		 * <summary>Compares two strings for equality, ignoring case and leading/trailing whitespace.</summary>
+		 * <param name="input">The first string to compare.</param>
+		 * <param name="other">The second string to compare.</param>
+		 * <returns>True if the strings are equal, ignoring case and leading/trailing whitespace; otherwise, false.</returns>
+		 */
+		public static bool LooseEquals(this string? input, string? other)
+		{
+			return string.Equals(input.ToLowerTrim(), other.ToLowerTrim(), StringComparison.CurrentCultureIgnoreCase);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# BachLib
+﻿# BachLib
+
+This is my personal library of useful functions and classes that I use for my C# projects. 
+It is a work in progress and I will be adding to it as I go along.
+
+## Current Features
+
+### Logger
+
+The Logger class allows unified logging to the console and/or to a file. 
+It supports different log levels and has some formatting options like timestamps and log levels.
+It can log colored messages on both Windows and Linux.
+
+### Utils
+
+The Utils class contains a collection of useful functions that I use in my projects.
+For now it's just some formatting and converting functions.
+
+### Extensions
+
+The Extensions class contains a collection of extension methods that I use in my projects.
+For now it's just some string extensions.
+
+## Usage
+
+*don't* :D
+
+<sub><sup>
+	They're all static classes so you can just call the functions directly. 👍
+</sup></sub>
